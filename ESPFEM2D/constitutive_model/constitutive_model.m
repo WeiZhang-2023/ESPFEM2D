82b7560afd332caa5374bff1db922155bf52a2f7
WeiZhang-2023 <142029273+WeiZhang-2023@users.noreply.github.com>
2023-11-29 23:19:32 +0800

    Delete ESPFEM2D directory

--- ESPFEM2D/constitutive_model/constitutive_model.m
+++
@@ -1,8 +0,0 @@
-function [stress,dpstrain_eq]=constitutive_model(mat_model,mat_props,stress,dstrain,pstrain_eq)
-if mat_model==1 %elas
-    stress=mat_model_elas(mat_props,stress,dstrain);
-    dpstrain_eq=0.;
-elseif mat_model==2 %DP
-    [stress,dpstrain_eq]=mat_model_DP(mat_props,stress,dstrain,pstrain_eq);
-end
-end
